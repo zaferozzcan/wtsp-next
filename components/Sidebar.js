e4f0fc6d65926aa630a97a1e95883b5413c7bb75
ZAFER OZCAN <zaferozzcan@gmail.com>
2021-04-15 21:31:48 -0400

    add header component includes icons and styling

---
+++ components/Sidebar.js
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+import { Avatar, IconButton, Button } from "@material-ui/core";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import ChatIcon from "@material-ui/icons/Chat";
+import SearchIcon from "@material-ui/icons/Search";
+
+export default function Sidebar() {
+  return (
+    <Container>
+      <Header>
+        <UserAvatar />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
+      <SideBarButton>Start New Chat</SideBarButton>
+      {/* user comps */}
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.div`
+  display: flex;
+  position: sticky;
+  top: 0;
+  background-color: white;
+  justify-content: space-between;
+  align-items: center;
+  padding: 15px;
+  border-bottom: 1px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+
+const IconsContainer = styled.div``;
+
+const Search = styled.div`
+  display: flex;
+  align-items: center;
+  padding: 20px;
+  borderradius: 2px;
+`;
+
+const SearchInput = styled.input`
+  outline-width: 0;
+  border: none;
+  flex: 1;
+`;
+
+const SideBarButton = styled(Button)`
+  border-top: 1px solid whitesmoke !important;
+  border-bottom: 1px solid whitesmoke !important;
+  width: 100%;
+  :hover {
+  }
+`;
